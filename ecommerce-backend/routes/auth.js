e49e65eaa8fe39aab9e24b0b30c38c6d529d1887
Betsinat Amare <amarebetsinat@gmail.com>
2025-10-15 09:57:38 +0300

    added middleware

--- ecommerce-backend/routes/auth.js
+++ ecommerce-backend/routes/auth.js
@@ -1,5 +1,10 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
+import auth from './middleware/auth.js';
+app.get('/api/private', auth, (req, res) => {
+  res.send('This is a protected route');
+});
+
 
 const router = express.Router();
 
